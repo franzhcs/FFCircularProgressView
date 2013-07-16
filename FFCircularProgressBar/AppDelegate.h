6b74a7e42371ecf43fef2f85b7f2913526e0bdfe
Fabiano Francesconi <fabiano.francesconi@gmail.com>
2013-07-16 10:04:10 +0200

    Initial Commit

---
+++ FFCircularProgressBar/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FFCircularProgressBar
+//
+//  Created by Fabiano Francesconi on 16/07/13.
+//  Copyright (c) 2013 Fabiano Francesconi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FFCircularProgressBar/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FFCircularProgressBar
+//
+//  Created by Fabiano Francesconi on 16/07/13.
+//  Copyright (c) 2013 Fabiano Francesconi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
