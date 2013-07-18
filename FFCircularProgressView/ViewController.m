39773537231c3bf0a6b31ae9a8424ae21e7efd65
Fabiano Francesconi <fabiano.francesconi@gmail.com>
2013-07-18 14:54:58 +0200

    Fixed demo (again)

--- FFCircularProgressView/ViewController.m
+++ FFCircularProgressView/ViewController.m
@@ -35,21 +35,20 @@
     
     [self.view addSubview:_circularPV];
 
-    double delayInSeconds = 1;
+    [_circularPV startSpinProgressBackgroundLayer];
+
+    double delayInSeconds = 2.5;
     dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
-    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
-        dispatch_async(dispatch_get_main_queue(), ^{
-            [_circularPV startSpinProgressBackgroundLayer];
-//            for (float i=0; i<1.1; i+=0.01F) {
-//                dispatch_async(dispatch_get_main_queue(), ^{
-//                    [_circularPV setProgress:i];
-//                });
-//                usleep(10000);
-//            }            
-        });
+    dispatch_after(popTime, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^(void){
+        for (float i=0; i<1.1; i+=0.01F) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [_circularPV setProgress:i];
+            });
+            usleep(10000);
+        }
     });
     
-    delayInSeconds = 5;
+    delayInSeconds = 2;
     popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
     dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
         [_circularPV stopSpinProgressBackgroundLayer];
